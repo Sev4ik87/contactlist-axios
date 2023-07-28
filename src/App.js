f2e5cd08dc464c9701cfe16e582ed765bc63a9d9
Sev4ik <se8v7a@gmail.com>
2023-07-28 10:28:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+import ContactList from './components/ContactList/ContactList';
+import ContactForm from './components/ContactForm/ContactForm';
+import api from './contact-service';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [contactForEdit, setContactForEdit] = useState(createEmptyContact());
+
+  function createEmptyContact() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+    };
+  }
+
+  
+
+  useEffect(() => {
+    api.get('/').then(({ data }) => {
+      data ? setContacts(data) : setContacts([]);
+    });
+  }, []);
+
+  const deleteContact = useCallback((id) => {
+    api.delete(`/${id}`)
+      .then(() => {
+        setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+      })
+      .catch((error) => {
+        console.error('Error deleting contact:', error);
+      });
+  }, []);
+
+  const addNewContact = useCallback(() => {
+    setContactForEdit(createEmptyContact());
+  }, []);
+
+  const selectContact = useCallback((contact) => {
+    setContactForEdit(contact);
+  }, []);
+
+  const createContact = useCallback(
+    (contact) => {
+      api.post('/', contact)
+        .then(({ data }) => {
+          contact.id = data.id;
+          setContacts((prevContacts) => [...prevContacts, contact]);
+          setContactForEdit(createEmptyContact());
+        })
+        .catch((error) => {
+          console.error('Error creating contact:', error);
+        });
+    },
+    []
+  );
+
+  const updateContact = useCallback(
+    (contact) => {
+      api.put(`/${contact.id}`, contact)
+        .then(() => {
+          setContacts((prevContacts) =>
+            prevContacts.map((item) => (item.id === contact.id ? contact : item))
+          );
+          setContactForEdit(contact);
+        })
+        .catch((error) => {
+          console.error('Error updating contact:', error);
+        });
+    },
+    []
+  );
+
+  const saveContact = useCallback(
+    (contact) => {
+      if (!contact.id) {
+        createContact(contact);
+      } else {
+        updateContact(contact);
+      }
+    },
+    [createContact, updateContact]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='header'>Contact List</h1>
+      <div className='main'>
+        <ContactList
+          contacts={contacts}
+          onDelete={deleteContact}
+          onAddContact={addNewContact}
+          onEditContact={selectContact}
+        />
+        <ContactForm
+          key={contactForEdit.id}
+          contactForEdit={contactForEdit}
+          onSubmit={saveContact}
+          onDelete={deleteContact}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
